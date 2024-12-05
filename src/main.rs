b3102c27af6bcb29d2738f70d3b3ffe73d90e309
carpenter <leonnjf@126.com>
2024-12-05 21:29:08 +0800

    1231

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use std::ffi::CString;
 use x11::xlib::*;
 use std::os::raw::c_int;
 use std::time::{Duration, Instant};
+use colorized::{Color, Colors};
 
 fn main() {
-    // 打开X11显示
     let display = unsafe { XOpenDisplay(ptr::null()) };
     let screen = unsafe { XDefaultScreen(display) };
     let root_window = unsafe { XRootWindow(display, screen) };
@@ -16,9 +16,8 @@ fn main() {
     let ww = unsafe { XDisplayWidth(display, screen) } as u32;
     let hh = unsafe { XDisplayHeight(display, screen) } as u32;
 
-    // 打印分辨率
-    println!("屏幕分辨率: {} x {}", ww, hh);
-    // 创建一个透明的覆盖窗口
+    println!("your resolution: {} x {}", ww, hh);
+
     let window = unsafe {
         XCreateSimpleWindow(
             display,
@@ -33,7 +32,6 @@ fn main() {
         )
     };
 
-    // 设置窗口为override-redirect类型
     let wm_type = unsafe { XInternAtom(display, CString::new("_NET_WM_WINDOW_TYPE").unwrap().as_ptr(), 0) };
     let wm_opacity = unsafe { XInternAtom(display, CString::new("_NET_WM_WINDOW_OPACITY").unwrap().as_ptr(), 0) };
 
@@ -50,8 +48,7 @@ fn main() {
             1,
         );
 
-        // 设置窗口透明度
-        let opacity: u32 = 0x80FFFFFF; // 50%透明度
+        let opacity: u32 = 0x80FFFFFF;
         XChangeProperty(
             display,
             window,
@@ -63,26 +60,21 @@ fn main() {
             1,
         );
 
-        // 设置窗口属性
         XSelectInput(display, window, ExposureMask | StructureNotifyMask | ButtonPressMask | ButtonReleaseMask | PointerMotionMask);
         XMapWindow(display, window);
         XFlush(display);
     }
 
-    // 变量以跟踪矩形的状态
     let mut is_drawing = false;
     let mut start_x = 0;
     let mut start_y = 0;
     let mut current_x = 0;
     let mut current_y = 0;
 
-    // 主循环
-    //let start = Instant::now();//start.elapsed() < Duration::new(30, 0)
     let mut isFin = false;
     let mut rect_width = 0;
     let mut rect_heigt = 0;
-    while !isFin { 
-        // 处理事件
+    while !isFin {
         unsafe {
             let mut event: XEvent = std::mem::zeroed();
             while XPending(display) > 0 {
@@ -90,9 +82,8 @@ fn main() {
 
                 match event.get_type() {
                     ButtonPress => {
-                        // 鼠标按下
-                        let button_event: &XButtonEvent = &event.button; // 使用方法获取字段
-                        if button_event.button == 1 { // 左键
+                        let button_event: &XButtonEvent = &event.button;
+                        if button_event.button == 1 {
                             is_drawing = true;
                             start_x = button_event.x;
                             start_y = button_event.y;
@@ -101,25 +92,23 @@ fn main() {
                         }
                     }
                     MotionNotify => {
-                        // 鼠标移动
                         if is_drawing {
-                            let motion_event: &XMotionEvent = &event.motion; // 使用方法获取字段
+                            let motion_event: &XMotionEvent = &event.motion;
                             current_x = motion_event.x;
                             current_y = motion_event.y;
 
                             // 重绘窗口
                             XClearWindow(display, window);
                             let gc = XCreateGC(display, window, 0, ptr::null_mut());
-                            XSetForeground(display, gc, 0xFFFF00); // 矩形颜色（黄色）
+                            XSetForeground(display, gc, 0xFFFF00);
                             XFillRectangle(display, window, gc, (start_x).try_into().unwrap(), (start_y).try_into().unwrap(), (current_x - start_x) as u32, (current_y - start_y) as u32);
                             XFreeGC(display, gc);
                             XFlush(display);
                         }
                     }
                     ButtonRelease => {
-                        // 鼠标松开
-                        let button_event: &XButtonEvent = &event.button; // 使用方法获取字段
-                        if is_drawing && button_event.button == 1 { // 左键
+                        let button_event: &XButtonEvent = &event.button;
+                        if is_drawing && button_event.button == 1 {
                             is_drawing = false;
                             rect_width = current_x - start_x;
                             rect_heigt = current_y - start_y;
@@ -133,23 +122,22 @@ fn main() {
             }
         }
 
-        // 刷新窗口
         unsafe {
             XFlush(display);
         }
 
-        // 控制绘制速率
         std::thread::sleep(Duration::from_millis(10));
     }
 
-    // 清理
     unsafe {
         XDestroyWindow(display, window);
         XCloseDisplay(display);
     }
-    println!("rect: ({}x{}), width: {}, height: {}", start_x, start_y, rect_width, rect_heigt);
-    //ffmpeg -video_size 1915x1075 -framerate 25 -f x11grab -i :0.0+154,308 -vf format=yuv420p abc.mp4
-    println!("ffmpeg -video_size {}x{} -framerate 25 -f x11grab -i :0.0+{},{} -vf format=yuv420p abc.mp4", rect_width, rect_heigt, start_x, start_y);
-    println!("pactl list sources short");
-    println!("ffmpeg -video_size {}x{} -framerate 25 -f x11grab -i :0.0+{},{} -f pulse -i bluez_output.98_DD_60_C6_9E_4F.1.monitor -vf format=yuv420p abc.mp4", rect_width, rect_heigt, start_x, start_y);
+    let rect = format!("rect: ({}x{}), width: {}, height: {}", start_x, start_y, rect_width, rect_heigt);
+    println!("{}", rect.color(Colors::BrightGreenFg));
+    let ffmpeg1 = format!("ffmpeg -video_size {}x{} -framerate 25 -f x11grab -i :0.0+{},{} -vf format=yuv420p abc.mp4", rect_width, rect_heigt, start_x, start_y);
+    println!("{}", ffmpeg1.color(Colors::BrightBlueBg).color(Colors::BlackFg));
+    println!("{}", "pactl list sources short".color(Colors::BrightBlueFg));
+    let ffmpeg2 = format!("ffmpeg -video_size {}x{} -framerate 25 -f x11grab -i :0.0+{},{} -f pulse -i bluez_output.98_DD_60_C6_9E_4F.1.monitor -vf format=yuv420p abc.mp4", rect_width, rect_heigt, start_x, start_y);
+    println!("{}", ffmpeg2.color(Colors::BrightBlueBg).color(Colors::BlackFg));
 }
